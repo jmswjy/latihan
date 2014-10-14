daeb9818893c08e5fdd0a91fda800ad73f9e9333
James Wijaya <jmswjy@gmail.com>
2014-10-14 19:53:01 +0700

    pertama buat
    Signed-off-by: James Wijaya <jmswjy@gmail.com>

---
+++ admin/header.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>.::Admin Dashboard::.</title>
+		<link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.css" />
+		<link rel="stylesheet" href="../assets/bootstrap/css/signin.css" />
+	</head>
+	<body>
+		
